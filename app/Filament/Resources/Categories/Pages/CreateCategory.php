7402f25692fa163adf4ff02e8b886c6cc1b8b74a
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-10-28 01:52:37 +0200

    add custom create notification for CreateCategory.php

--- app/Filament/Resources/Categories/Pages/CreateCategory.php
+++ app/Filament/Resources/Categories/Pages/CreateCategory.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\Categories\Pages;
 
 use App\Filament\Resources\Categories\CategoryResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCategory extends CreateRecord
 {
     protected static string $resource = CategoryResource::class;
+    
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title(__( 'panel.category_created_successfully' ))
+            ->icon('heroicon-o-check-circle')
+            ->iconColor('success');
+    }
 }
