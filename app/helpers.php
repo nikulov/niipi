e50279d8a14e078c7a9fbbdb46837f15127344ab
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-10-27 15:19:09 +0200

    add __date to helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,10 @@
 <?php
+
+use Illuminate\Support\Carbon;
+
+if (!function_exists('__date')) {
+    function __date($date = null)
+    {
+        return $date ? Carbon::parse($date)->format('d.m.Y') : '';
+    }
+}
