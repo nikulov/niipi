ac2d8bea52b79bdbe7b1f30e697147888b812077
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-10-27 20:57:54 +0200

    update filament PostsTable.php add columns, search

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\Posts\Tables;
 
+use Carbon\Carbon;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class PostsTable
@@ -14,7 +17,28 @@ class PostsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title')->label(__('Title'))
+                    ->limit(50)
+                    ->sortable(),
+                TextColumn::make('status')->label(__('Status'))
+                    ->badge(),
+                TextColumn::make('published_at')->label(__('Published At'))
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('created_at')->label(__('Created At'))
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')->label(__('Updated At'))
+                    ->sortable()
+                    ->formatStateUsing(function ($state) {
+                        if (! $state) {return null;}
+                        $date = Carbon::parse($state);
+                        $hours = $date->diffInHours(now());
+                        if ($hours > 6) {
+                            return $date->translatedFormat('d.m.Y H:i');
+                        }
+                        return $date->diffForHumans();
+                    }),
             ])
             ->filters([
                 //
@@ -22,11 +46,14 @@ class PostsTable
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->searchable(['title'])
+            ->searchPlaceholder(__('Search title'));
     }
 }
