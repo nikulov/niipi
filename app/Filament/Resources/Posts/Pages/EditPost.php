08947c0acef4664cc411295c8cd4e9af409693f7
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-10-28 00:11:51 +0200

    add save button and update notification message for EditPost.php

--- app/Filament/Resources/Posts/Pages/EditPost.php
+++ app/Filament/Resources/Posts/Pages/EditPost.php
@@ -5,17 +5,30 @@ namespace App\Filament\Resources\Posts\Pages;
 use App\Filament\Resources\Posts\PostResource;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Actions;
 
 class EditPost extends EditRecord
 {
     protected static string $resource = PostResource::class;
-
+    
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('save')
+                ->label(__('Save'))
+                ->action(fn() => $this->save()),
             ViewAction::make(),
             DeleteAction::make(),
         ];
+    }
+    
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+        ->title(__( 'Post saved successfully' ))
+        ->icon('heroicon-o-check-circle')
+        ->iconColor('success');
     }
 }
