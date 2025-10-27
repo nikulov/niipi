bb2b4c2ed998082137b78c5da5f3ede68e6cde95
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-10-28 01:51:48 +0200

    add custom create notification for CreatePost.php

--- app/Filament/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+    
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title(__( 'panel.post_created_successfully' ))
+            ->icon('heroicon-o-check-circle')
+            ->iconColor('success');
+    }
 }
