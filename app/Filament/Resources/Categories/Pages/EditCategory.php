7135006bbbd949ec7e929ea6e3b286c1ae021d15
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-10-28 00:12:07 +0200

    add save button and update notification message for EditCategory.php

--- app/Filament/Resources/Categories/Pages/EditCategory.php
+++ app/Filament/Resources/Categories/Pages/EditCategory.php
@@ -3,19 +3,32 @@
 namespace App\Filament\Resources\Categories\Pages;
 
 use App\Filament\Resources\Categories\CategoryResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditCategory extends EditRecord
 {
     protected static string $resource = CategoryResource::class;
-
+    
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('save')
+                ->label(__('Save'))
+                ->action(fn() => $this->save()),
             ViewAction::make(),
             DeleteAction::make(),
         ];
+    }
+    
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title(__( 'Category saved successfully' ))
+            ->icon('heroicon-o-check-circle')
+            ->iconColor('success');
     }
 }
