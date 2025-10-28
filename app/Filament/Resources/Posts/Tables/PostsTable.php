6c983ba9b3d663e1dcea164094736da49c110710
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-10-28 03:40:31 +0200

    update PostsTable.php add filter by category, status and created_ar range, add icon status

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -2,14 +2,25 @@
 
 namespace App\Filament\Resources\Posts\Tables;
 
+use App\Enums\PostStatus;
+use App\Models\Post;
 use Carbon\Carbon;
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Forms\Components\DatePicker;
+use Filament\Schemas\Components\Group;
+use Filament\Support\Enums\IconPosition;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class PostsTable
 {
@@ -23,9 +34,10 @@ class PostsTable
                     ->wrap()
                     ->sortable(),
                 TextColumn::make('status')->label(__('panel.status'))
-                    ->badge(),
+                    ->badge()
+                    ->icon(fn (PostStatus $state) => $state->getIcon()),
                 TextColumn::make('category.name')->label(__('panel.category'))
-                ->badge(),
+                    ->badge(),
                 TextColumn::make('published_at')->label(__('panel.published_at'))
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
@@ -47,8 +59,47 @@ class PostsTable
                     }),
             ])
             ->filters([
-                //
-            ])
+                SelectFilter::make('status')->label(__('panel.status'))
+                    ->columnSpan(3)
+                    ->options(PostStatus::class)
+                    ->multiple()
+                    ->preload(),
+                SelectFilter::make('category_id')->label(__('panel.category'))
+                    ->columnSpan(3)
+                    ->relationship('category', 'name')
+                    ->multiple()
+                    ->preload(),
+                Filter::make('created_at')
+                    ->schema([
+                        DatePicker::make('created_from')->label(__('panel.created_from'))
+                            ->columnSpan(3)
+                            ->maxDate(now())
+                            ->minDate(fn() => Post::min('created_at')),
+                        DatePicker::make('created_until')->label(__('panel.created_until'))
+                            ->columnSpan(3)
+                            ->maxDate(now()),
+                    ])
+                    ->columns(6)
+                    ->columnSpan(6)
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_from'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['created_until'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    })
+            
+            ], layout: FiltersLayout::AboveContent)->deferFilters(false)
+            ->filtersFormColumns(12)
+            ->filtersTriggerAction(
+                fn(Action $action) => $action
+                    ->button()
+                    ->label(__('panel.filter')),
+            )
             ->recordActions([
                 ViewAction::make()->label(__(''))
                     ->iconSize('md')
