6418d91e1da6724eafb35b8d33c9d24784127bef
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-10-28 02:17:31 +0200

    add redirect after update post to post_list and add post-link to norification

--- app/Filament/Resources/Posts/Pages/EditPost.php
+++ app/Filament/Resources/Posts/Pages/EditPost.php
@@ -3,11 +3,13 @@
 namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Actions;
+use http\Message\Body;
 
 class EditPost extends EditRecord
 {
@@ -16,7 +18,7 @@ class EditPost extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\Action::make('save')
+            Action::make('save')
                 ->label(__('panel.save'))
                 ->action(fn() => $this->save()),
             ViewAction::make(),
@@ -27,8 +29,18 @@ class EditPost extends EditRecord
     protected function getSavedNotification(): ?Notification
     {
         return Notification::make()
-        ->title(__( 'panel.post_saved_successfully' ))
-        ->icon('heroicon-o-check-circle')
-        ->iconColor('success');
+            ->title(__('panel.post_saved_successfully'))
+            ->icon('heroicon-o-check-circle')
+            ->iconColor('success')
+            ->actions([
+                Action::make('view')->label(__('panel.view_post'))
+                    ->url(route('filament.admin.resources.posts.view', ['record' => $this->record]))
+                    ->openUrlInNewTab(),
+            ]);
+    }
+    
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
     }
 }
