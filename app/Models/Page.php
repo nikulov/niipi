0478eb81befd7496c92b72400e3ae7cb4e2d9776
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-12-16 17:16:38 +0200

    add model Page

---
+++ app/Models/Page.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\PageStatus;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Page extends Model
+{
+    protected $table = 'sections';
+    
+    protected $fillable = [
+        'title',
+        'slug',
+        'status',
+        'published_at',
+        'top_section',
+        'main_section',
+        'bottom_section',
+    
+    ];
+    
+    protected $casts = [
+        'status' => PageStatus::class,
+        'top_section' => 'array',
+        'main_section' => 'array',
+        'bottom_section' => 'array',
+        'published_at' => 'datetime',
+    ];
+    
+    public function setSlugAttribute(?string $value): void
+    {
+        $this->attributes['slug'] = $value ? ltrim($value, '/') : null;
+    }
+    
+}
