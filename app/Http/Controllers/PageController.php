dd3cdf4580cd83cb7811c6481a1c361ebf2daed1
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-12-17 15:34:44 +0200

    add PageController.php

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Page;
+
+class PageController extends Controller
+{
+    
+    public function index(?string $slug = null)
+    {
+        if ($slug === null || $slug === '/' || $slug === 'home') {
+            $slug = 'home';
+        }
+        
+        $slug = ltrim($slug, '/');
+        $page = Page::query()->where('slug', $slug)->firstOrFail();
+        
+        return view('layout.page', compact('page'));
+    }
+    
+}
