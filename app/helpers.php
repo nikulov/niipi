fddee2352e4045960ddd93ad34cdb256152acc68
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-12-17 17:03:50 +0200

    add helpers public_assets

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,28 @@
 <?php
+
+
+use Illuminate\Support\Facades\Storage;
+
+if (!function_exists('public_asset')) {
+    function public_asset(?string $path): ?string
+    {
+        if (!$path) {
+            return null;
+        }
+        
+        if (
+            str_starts_with($path, 'http://')
+            || str_starts_with($path, 'https://')
+            || str_starts_with($path, '/')
+        )
+        {
+            return $path;
+        }
+        
+        try {
+            return Storage::disk('public')->url($path);
+        } catch (\Throwable $e) {
+            return null;
+        }
+    }
+}
