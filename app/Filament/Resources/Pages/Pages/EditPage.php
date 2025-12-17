2d354e79c05c1b0f529200c8b1c212b2f1e731ae
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-12-17 17:03:19 +0200

    add save button and custom save notification

--- app/Filament/Resources/Pages/Pages/EditPage.php
+++ app/Filament/Resources/Pages/Pages/EditPage.php
@@ -3,19 +3,33 @@
 namespace App\Filament\Resources\Pages\Pages;
 
 use App\Filament\Resources\Pages\PageResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPage extends EditRecord
 {
     protected static string $resource = PageResource::class;
-
+    
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('save')
+                ->label('Сохранить')
+                ->action(fn () => $this->save()),
             ViewAction::make(),
             DeleteAction::make(),
         ];
+    }
+    
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->title('Страница успешно сохранена') // свой текст
+            ->icon('heroicon-o-check-circle')     // иконка
+            ->iconColor('success')                // цвет иконки
+            ->body('Все изменения применены и сохранены.'); // дополнительный текст
     }
 }
