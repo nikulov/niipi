230d3c71557dd9f09537ad0d7dbb77b4879b97da
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-12-17 15:36:20 +0200

    add home and other pages routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('/{slug}', [PageController::class,'index'])
+    ->where('slug','^(?!admin|api|login|register).+')
+    ->name('page.index');
