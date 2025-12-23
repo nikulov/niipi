4bd800aac727c13ff22121eeda5a79583f552cda
Andrei Nikulov <andrei.nikulov@gmail.com>
2025-12-23 13:39:32 +0200

    change menu position

--- app/Filament/Resources/GlobalSettings/GlobalSettingResource.php
+++ app/Filament/Resources/GlobalSettings/GlobalSettingResource.php
@@ -25,8 +25,7 @@ class GlobalSettingResource extends Resource
     
     protected static string|null|\UnitEnum $navigationGroup = 'Настройки';
     
-    protected static ?int $navigationSort = 2;
-    
+    protected static ?int $navigationSort = 3;
     public static function form(Schema $schema): Schema
     {
         return GlobalSettingForm::configure($schema);
