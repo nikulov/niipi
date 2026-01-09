c2823d3fdcc23a2b4b81093f34f959019f5ebaf7
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-09 13:46:49 +0200

    add route for project detail page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ContentController::class, 'page'])->name('home');
 Route::get('/news/{slug}', [ContentController::class, 'post'])->name('news.show');
+Route::get('/projects/{slug}', [ContentController::class, 'project'])->name('projects.show');
 
 Route::get('/{slug}', [ContentController::class, 'page'])
     ->where('slug', '^(?!admin|api|login|register).+')
