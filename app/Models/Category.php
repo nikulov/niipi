08a7caf7ef387088e6bab19d5c419a8886f8a021
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-09 12:47:45 +0200

    update category model and add type enum and scope methods

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 use App\Enums\CategoryStatus;
+use App\Enums\CategoryType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Query\Builder;
 
 class Category extends Model
 {
     protected $fillable = [
         'name',
+        'type',
         'slug',
         'status',
         'meta_title',
@@ -18,11 +21,27 @@ class Category extends Model
     ];
     
     protected $casts = [
+        'type' => CategoryType::class,
         'status' => CategoryStatus::class,
     ];
     
     public function posts(): BelongsToMany
     {
-        return $this->belongsToMany(Post::class);
+        return $this->belongsToMany(Post::class, 'category_post');
+    }
+    
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'category_project');
+    }
+    
+    public function scopePosts(Builder $query): Builder
+    {
+        return $query->where('type', CategoryType::Posts);
+    }
+    
+    public function scopeProjects(Builder $query): Builder
+    {
+        return $query->where('type', CategoryType::Projects);
     }
 }
