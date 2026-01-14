87c3506875f37d583f539396d7655927c2ec6dc4
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-14 18:20:28 +0200

    refactor category list: adjust base path logic and update category link generation for improved URL handling

--- resources/views/components/sections/category-list.blade.php
+++ resources/views/components/sections/category-list.blade.php
@@ -1,11 +1,11 @@
-@php($basePath = '/' . request()->segment(1))
+@php($basePath = request()->segment(1))
 
 <div class="w-full max-w-1242 mx-auto my-inner-section-y px-inner-section-x">
     <div class="w-full border-t border-accent pb-2"></div>
     <span class="pr-1 text-accent text-small uppercase font-bold">Категории: </span>
     
     @foreach($categories as $slug => $category)
-        <a  href="{{ $basePath . '?category=' . urlencode($slug) }}"
+        <a  href="{{ '/' . $basePath . '?' . $basePath .'Category=' . urlencode($slug) }}"
             class="text-accent hover:text-accent-add text-small underline"
         >
             {{$category}}
