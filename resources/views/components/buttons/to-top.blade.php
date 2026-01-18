8b7d2679b84481ba4cb610857e12926f6a8713e0
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-18 12:41:11 +0200

    update position for to-top button component

--- resources/views/components/buttons/to-top.blade.php
+++ resources/views/components/buttons/to-top.blade.php
@@ -3,7 +3,7 @@
         x-init="init()"
         x-show="visible"
         x-transition.opacity
-        class="fixed bottom-6 right-2 z-50 to-top"
+        class="fixed bottom-6 z-50 to-top right-2 2xl:right-[calc((100vw-1600px)/2)]"
 >
     <button @click="scrollTop" id="to-top-btn" class="rounded-full cursor-pointer">
         <x-icon.icon-to-top class="w-16 h-16 transition-colors duration-300 ease-in-out"/>
