6e4bf0ff0bccf45ced4551a9384486d2c8f5c891
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-24 18:54:13 +0200

    change key for dark theme in local storage to themeSite

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -29,7 +29,6 @@
         </style>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
         @livewireStyles
 
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
