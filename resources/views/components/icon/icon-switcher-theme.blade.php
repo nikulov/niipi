20b26a80e4e7668e556af6aa6606b4cc91208f83
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-24 20:16:36 +0200

    update icon components for switcher theme and gallery slider

--- resources/views/components/icon/icon-switcher-theme.blade.php
+++ resources/views/components/icon/icon-switcher-theme.blade.php
@@ -1,10 +1,13 @@
-<div class="group border-accent dark:border-accent-add-dark relative z-20 h-6 w-6 overflow-hidden rounded-full border">
+<div
+    class="group border-accent dark:border-accent-add-dark relative z-20 h-6 w-6 overflow-hidden rounded-full border drop-shadow-[0_0_2px_#60c0c3]"
+>
     <x-icon.icon-switcher-empty class="fill-accent absolute -top-px -left-px max-h-6 min-h-6 max-w-6 min-w-6" />
+
     <div
         class="absolute -top-px -left-px h-6 w-6 -translate-x-2 rounded-full bg-[#2e3445] transition-transform duration-200 group-hover:translate-x-0 dark:translate-x-0"
     ></div>
 
     <x-icon.icon-switcher-fill
-        class="fill-accent-add absolute -top-px -left-px max-h-6 min-h-6 max-w-6 min-w-6 -translate-x-10 overflow-hidden text-[#fbfcfd] transition-transform duration-200 dark:-translate-x-2 group-hover:dark:translate-x-0"
+        class="absolute -top-px -left-px max-h-6 min-h-6 max-w-6 min-w-6 -translate-x-10 overflow-hidden fill-[#EFF0F2] text-[#EFF0F2] transition-transform duration-200 dark:-translate-x-2 group-hover:dark:translate-x-0"
     />
 </div>
--- resources/views/components/other/social-icon.blade.php
+++ resources/views/components/other/social-icon.blade.php
@@ -1,11 +1,8 @@
-@props(
-    [
-        'url' => '#',
-        'iconUrl' => '#'
-        
-]
-)
+@props([
+    'url' => '#',
+    'iconUrl' => '#',
+])
 
-<a href="{{$url}}" class="flex h-10 w-10 items-center justify-center">
+<a href="{{ $url }}" class="dark:[&>svg]:fill-white-dark! flex h-10 w-10 items-center justify-center [&>svg]:fill-white!">
     {!! file_get_contents(public_asset($iconUrl)) !!}
 </a>
