b23a720ba2c64a0d6acf0d401aec005d141408da
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-24 16:26:38 +0200

    update category selection components style

--- resources/views/components/other/categories-list.blade.php
+++ resources/views/components/other/categories-list.blade.php
@@ -3,19 +3,15 @@
     'active' => null,
 ])
 
-<div {{ $attributes->merge(['class' => 'relative hidden md:flex flex-wrap justify-center gap-8 mb-20']) }}>
-    @foreach($items as $item)
+<div {{ $attributes->merge(['class' => 'relative font-century hidden md:flex flex-wrap justify-center gap-8 mb-20']) }}>
+    @foreach ($items as $item)
         <button
-                type="button"
-                wire:click="setCategory({{ $item['slug'] ? "'{$item['slug']}'" : 'null' }})"
-                class="relative cursor-pointer uppercase hover:text-[#5B8EAE]
-            {{ $item['slug'] ? 'pl-8 before:content-[""] before:absolute before:-left-px before:w-1 before:h-5 before:bg-[url("/resources/images/layout/acc-dots-dark.svg")]' : '' }}
-            {{ $active === $item['slug'] ? 'text-accent dark:text-accent-add-dark font-bold' : 'text-primary dark:text-accent-add-dark' }}"
+            type="button"
+            wire:click="setCategory({{ $item['slug'] ? "'{$item['slug']}'" : 'null' }})"
+            class="{{ $item['slug'] ? 'pl-8 before:content-[""] transition-colors duration-100 before:absolute before:-left-px before:w-1 before:h-5 before:bg-[url("/resources/images/layout/acc-dots-dark.svg")]' : '' }} {{ $active === $item['slug'] ? 'text-accent dark:text-accent-add-dark [text-shadow:0.04em_0_currentColor,-0.04em_0_currentColor]' : 'text-primary dark:text-accent-add-dark' }} relative cursor-pointer uppercase hover:text-[#5B8EAE] hover:[text-shadow:0.04em_0_currentColor,-0.04em_0_currentColor]"
         >
             {{ $item['name'] }}
-            <span class="opacity-60">
-                [{{ $item['count'] }}]
-            </span>
+            <span class="opacity-60">[{{ $item['count'] }}]</span>
         </button>
     @endforeach
 </div>
--- resources/views/components/other/categories-select.blade.php
+++ resources/views/components/other/categories-select.blade.php
@@ -5,16 +5,11 @@
 
 <div {{ $attributes->merge(['class' => 'mb-12 md:hidden']) }}>
     <select
-            class="w-full border border-border focus:border-accent focus-visible:outline-0 bg-white px-4 py-3 uppercase text-primary"
-            wire:change="setCategory($event.target.value || null)"
+        class="border-border focus:border-accent text-text font-century dark:bg-background-dark dark:text-text-dark w-full border bg-white px-4 py-3 uppercase focus-visible:outline-0"
+        wire:change="setCategory($event.target.value || null)"
     >
-        @foreach($items as $item)
-            <option
-                    value="{{ $item['slug'] }}"
-                    @selected($active === $item['slug'])
-            >
-                {{ $item['name'] }} [{{ $item['count'] }}]
-            </option>
+        @foreach ($items as $item)
+            <option value="{{ $item['slug'] }}" @selected($active === $item['slug'])>{{ $item['name'] }} [{{ $item['count'] }}]</option>
         @endforeach
     </select>
 </div>
