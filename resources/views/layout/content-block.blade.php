09f786299917da434ed922a002340af446ddfda1
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-28 22:56:15 +0200

    add main-section-border component and adjust layout structure to include border elements and adjust main-section content layout

--- resources/views/layout/content-block.blade.php
+++ resources/views/layout/content-block.blade.php
@@ -9,34 +9,8 @@
             style="background-image: url({{ public_asset($bgForMainSection) }});"
         @endif
     >
-        <div
-            class="bg-accent absolute top-0 flex h-4.5 w-full items-start justify-between"
-            style="clip-path: polygon(0 0, 100% calc(100% - 18px), calc(100% - 18px) 100%, 20px 100%, 0 calc(100% - 18px))"
-        >
-            <div
-                class="absolute top-0 left-1/2 flex h-4.5 w-18 -translate-x-1/2 items-center justify-center bg-[url('/resources/images/layout/item-top-content.svg')] bg-no-repeat"
-            ></div>
-
-            <div
-                class="absolute top-1/2 right-2.5 flex h-1.5 w-20 -translate-y-1/2 items-center justify-center bg-[url('/resources/images/layout/item-top-right-content.svg')] bg-no-repeat"
-            ></div>
-        </div>
-
-        @yield("main_section")
-
-        <div
-            class="bg-accent absolute right-0 -bottom-4.5 flex h-4.5 w-[60%] items-start justify-between md:w-[40%]"
-            style="clip-path: polygon(0 0, 100% calc(100% - 18px), 100% 100%, 20px 100%, 0 calc(100% - 18px))"
-        >
-            <div
-                class="absolute top-1/2 left-4 flex h-1.5 w-20 -translate-y-1/2 items-center justify-center bg-[url('/resources/images/layout/item-bottom-left-content.svg')] bg-no-repeat"
-            ></div>
-
-            <div
-                class="absolute top-1/2 right-2 hidden h-2.5 w-40 -translate-y-1/2 bg-[url('/resources/images/layout/item-bottom-right-content.svg')] bg-no-repeat md:block"
-            ></div>
-        </div>
-    
+        <x-layout.main-section-border>
+            @yield("main_section")
+        </x-layout.main-section-border>
     </div>
-
 </div>
