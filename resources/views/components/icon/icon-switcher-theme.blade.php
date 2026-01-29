2b01276c83f3e8af23d5c5b99fe6b09f2ba72630
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-01-29 16:32:12 +0200

    update icon switcher layout

--- resources/views/components/icon/icon-switcher-theme.blade.php
+++ resources/views/components/icon/icon-switcher-theme.blade.php
@@ -1,13 +1,15 @@
-<div
-    class="group border-accent dark:border-accent-add-dark relative z-20 h-6 w-6 overflow-hidden rounded-full border drop-shadow-[0_0_2px_#60c0c3]"
->
-    <x-icon.icon-switcher-empty class="fill-accent absolute -top-px -left-px max-h-6 min-h-6 max-w-6 min-w-6" />
+<div class="relative z-20 h-6 w-6">
+    <div class="absolute inset-0 rounded-full shadow-[0_0_2px_#60c0c3]"></div>
 
-    <div
-        class="absolute -top-px -left-px h-6 w-6 -translate-x-2 rounded-full bg-[#2e3445] transition-transform duration-200 group-hover:translate-x-0 dark:translate-x-0"
-    ></div>
+    <div class="group border-accent dark:border-accent-add-dark relative h-6 w-6 overflow-hidden rounded-full border">
+        <x-icon.icon-switcher-empty class="fill-accent absolute -top-px -left-px max-h-6 min-h-6 max-w-6 min-w-6" />
 
-    <div
-        class="absolute -top-px -left-px max-h-6 min-h-6 max-w-6 min-w-6 -translate-x-10 overflow-hidden rounded-full bg-[#EFF0F2] transition-transform duration-200 dark:-translate-x-2 group-hover:dark:translate-x-0"
-    ></div>
+        <div
+            class="absolute -top-px -left-px h-6 w-6 -translate-x-2 rounded-full bg-[#2e3445] transition-transform duration-200 group-hover:translate-x-0 dark:translate-x-0"
+        ></div>
+
+        <div
+            class="absolute -top-px -left-px max-h-6 min-h-6 max-w-6 min-w-6 -translate-x-10 overflow-hidden rounded-full bg-[#EFF0F2] transition-transform duration-200 dark:-translate-x-2 group-hover:dark:translate-x-0"
+        ></div>
+    </div>
 </div>
