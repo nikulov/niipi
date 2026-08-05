69d4e240eff5915916b3cfcf3cb0617fe294bfc7
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-08-05 15:09:41 +0200

    move related-thematic card title below the image
    The title used to sit on top of the thumbnail over a permanent dark
    overlay. Split the card into an image box and a text block underneath,
    and keep the overlay as a hover-only effect.

--- resources/views/components/sections/related-thematic.blade.php
+++ resources/views/components/sections/related-thematic.blade.php
@@ -16,22 +16,23 @@
 
     <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5">
         @foreach ($cards as $card)
-            <a href="{{ $card['url'] }}" class="group bg-primary/10 relative block aspect-square w-full overflow-hidden">
-                @if (! empty($card['thumbnail']))
-                    <img
-                        src="{{ $card['thumbnail'] }}"
-                        alt="{{ $card['title'] ?: $fallbackAlt }}"
-                        loading="lazy"
-                        class="h-full w-full object-cover transition duration-300 group-hover:scale-105"
-                    />
-                @endif
+            <a href="{{ $card['url'] }}" class="group flex w-full flex-col gap-3">
+                <div class="bg-primary/10 relative aspect-square w-full overflow-hidden">
+                    @if (! empty($card['thumbnail']))
+                        <img
+                            src="{{ $card['thumbnail'] }}"
+                            alt="{{ $card['title'] ?: $fallbackAlt }}"
+                            loading="lazy"
+                            class="h-full w-full object-cover transition duration-300 group-hover:scale-105"
+                        />
+                    @endif
 
-                <div class="bg-primary/40 group-hover:bg-primary/60 absolute inset-0 transition"></div>
-                <div class="absolute inset-x-0 bottom-0 p-3">
-                    <p class="text-small line-clamp-3 font-bold text-white">
-                        {{ $card['title'] }}
-                    </p>
+                    <div class="bg-primary/0 group-hover:bg-primary/40 absolute inset-0 transition"></div>
                 </div>
+
+                <p class="text-small text-primary dark:text-white-dark line-clamp-3 font-bold">
+                    {{ $card['title'] }}
+                </p>
             </a>
         @endforeach
     </div>
