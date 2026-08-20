5cf6c6d147052d16e3caa75fdd7565d814932d78
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-08-20 10:31:15 +0200

    set real contact links in the mobile menu contact block

--- resources/views/components/menu/mobile-contact-block.blade.php
+++ resources/views/components/menu/mobile-contact-block.blade.php
@@ -1,13 +1,13 @@
 <div class="my-inner-section-y px-inner-section-x flex items-center justify-between gap-6">
-    <x-menu.mobile-contact-item url="#">
+    <x-menu.mobile-contact-item url="tel:+74952427707">
         <x-icon.icon-mobile class="dark:fill-white-dark h-9 w-9 fill-white" />
     </x-menu.mobile-contact-item>
 
-    <x-menu.mobile-contact-item url="#">
+    <x-menu.mobile-contact-item url="mailto:info@niipi.ru">
         <x-icon.icon-at class="dark:fill-white-dark h-9 w-9 fill-white" />
     </x-menu.mobile-contact-item>
 
-    <x-menu.mobile-contact-item url="#">
+    <x-menu.mobile-contact-item url="https://t.me/niipigrad">
         <x-logo.logo-tg class="dark:fill-white-dark h-9 w-9 fill-white" />
     </x-menu.mobile-contact-item>
 </div>
