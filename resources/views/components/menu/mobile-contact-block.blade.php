da90f4d984f1e13350726a5c452a8d58d08ce6e7
Andrei Nikulov <andrei.nikulov@gmail.com>
2026-08-27 11:09:12 +0200

    use the phone handset icon for the mobile contact link

--- resources/views/components/menu/mobile-contact-block.blade.php
+++ resources/views/components/menu/mobile-contact-block.blade.php
@@ -1,6 +1,6 @@
 <div class="my-inner-section-y px-inner-section-x flex items-center justify-between gap-6">
     <x-menu.mobile-contact-item url="tel:+74952427707">
-        <x-icon.icon-mobile class="dark:fill-white-dark h-9 w-9 fill-white" />
+        <x-icon.icon-call-phone class="dark:fill-white-dark h-9 w-9 fill-white" />
     </x-menu.mobile-contact-item>
 
     <x-menu.mobile-contact-item url="mailto:info@niipi.ru">
